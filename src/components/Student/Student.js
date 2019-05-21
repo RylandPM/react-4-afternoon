d6728e02f8221536d360b883b60eca4381407d62
Ryland McGee <mcgee.reilly@gmail.com>
2019-05-21 15:17:22 -0800

    done bar BD

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Student extends Component {
   constructor() {
-    super()
+    super();
 
+    this.state = {
+      studentInfo: {}
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(result => {
+        this.setState({
+          studentInfo: result.data
+        });
+      });
   }
 
   render() {
+    const { studentInfo } = this.state;
     return (
       <div className="box">
-        <h1>Student</h1>
+        <h1>
+          {studentInfo.first_name} {studentInfo.last_name}
+        </h1>
+        <h3>Grade: {studentInfo.grade}</h3>
+        <h3>Email: {studentInfo.email}</h3>
       </div>
-    )
+    );
   }
 }
